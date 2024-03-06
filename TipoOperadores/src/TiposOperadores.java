a7d22d4b7e9a1bbf76db89396f6c445d77a58955
Luciano Ribeiro <lucianofloripa@outlook.com>
2024-03-06 15:08:07 -0300

    Teste Tipos de operadores - if - else - condição ternária

---
+++ TipoOperadores/src/TiposOperadores.java
@@ -0,0 +1,24 @@
+public class TiposOperadores {
+
+public static void main(String[] args) {
+    
+    int a, b;
+   
+    a = 5;
+    b = 6;
+
+    String Resultado = "";
+        if (a==b)
+            Resultado = "verdadeiro";
+        else
+            Resultado = "false";
+
+
+    System.out.println(Resultado);        
+
+
+}
+
+
+
+}
